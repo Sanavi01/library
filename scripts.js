c4dbed019d429cc391f36716206d65fa39ed249f
Santiago Avila <sanavila2002@gmail.com>
2022-05-18 18:22:14 -0500

    Added the right part of the library; - Created a welcome for users - Created the table where the information will added - Created a system to create tables when the user add a book ------------------------------ Changes: - In the aspect of the library - Now the variables titleInput, autorInput, pagesInput, readInput are written like global variables

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
+const table = document.getElementById('table-books')
+
+let titleInput = document.getElementById('title')
+let autorInput = document.getElementById('autor')
+let pagesInput = document.getElementById('pages')
+let readInput = document.getElementById('readornot')
+let row = 0 
+
 const addBook = document.getElementById('addBook')
 addBook.addEventListener('click', () => {
     addBookToLibrary()
 })
 
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(title, autor, pages, readornot) {
     this.title = title
@@ -13,10 +21,9 @@ function Book(title, autor, pages, readornot) {
 }
 
 function addBookToLibrary() {
-    const titleInput = document.getElementById('title').value;
-    const autorInput = document.getElementById('autor').value;
-    const pagesInput = document.getElementById('pages').value;
-    const readInput = document.getElementById('readornot')
+    titleInput = document.getElementById('title').value;
+    autorInput = document.getElementById('autor').value;
+    pagesInput = document.getElementById('pages').value;
     if (readInput.checked === true) {
         readInput.value = "Yes"
     } else {
@@ -25,4 +32,21 @@ function addBookToLibrary() {
     const book = new Book(titleInput, autorInput, pagesInput, readInput.value)
     myLibrary.push(book)
     console.log(myLibrary)
+
+    createTables()
+}
+
+function createTables(){
+    let td = 0
+    let column = 1
+    const tr= document.createElement('tr')
+    table.appendChild(tr)
+    for(td = 0; td < 5; td++){
+        const tdDivs = document.createElement('td')
+        tdDivs.classList.add(`position${row}-${column}`)
+        column = column + 1
+        tr.appendChild(tdDivs)
+    }
+    row = row + 1 
+    console.log(row)
 }
