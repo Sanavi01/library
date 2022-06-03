54c848330114e7c4ccfb8e6d753cc95120b6e923
Santiago Avila <sanavila2002@gmail.com>
2022-06-03 17:59:24 -0500

    Created a function for delete books from the library: - Now a button call the function to delete the book

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ let pagesInput = document.getElementById('pages')
 let readInput = document.getElementById('readornot')
 let rowCreateTable = 0
 let rowSelectTable = 0
-let book = 0
 
 const addBook = document.getElementById('addBook')
 addBook.addEventListener('click', () => {
@@ -72,13 +71,70 @@ function showBook() {
     selectorTable = `.position${rowSelectTable}-${columnSelectTable}`
     const readOrNotTable = document.querySelector(selectorTable)
     readOrNotTable.textContent = myLibrary[book].read
-    //Finish the table
+    //delete button
+    columnSelectTable = columnSelectTable + 1
+    selectorTable = `.position${rowSelectTable}-${columnSelectTable}`
+    const deleteTable = document.querySelector(selectorTable)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('deleteBtn')
+    deleteTable.appendChild(deleteBtn)
+    //Finish the table (next book)
     rowSelectTable = rowSelectTable + 1
-    book = book + 1
+    selectDeleteBtn()
+}
+
+function selectDeleteBtn() {
+    const selectedDeleteBtn = document.querySelectorAll('.deleteBtn')
+    selectedDeleteBtn.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', () => {
+            deleteBtn.parentElement.parentElement.remove()
+        })
+    })
+}
+
+/*
+PARA ELIMINAR EL LIBRO DEL ARRAY
+
+function deleteBookFromArray() {
+    const selectedDeleteBtn = document.querySelectorAll('.deleteBtn')
+    selectedDeleteBtn.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', () => {
+            let firstStep = deleteBtn.parentElement.parentElement.firstChild
+            let secondStep = firstStep.textContent
+            let indexInArray = myLibrary.findIndex((book) =>  book.title === secondStep)    
+            console.log(indexInArray)
+            myLibrary.splice(indexInArray,1)
+            console.log(myLibrary)
+        })
+    })
 }
 
 
+function identifyDeleteButtons(){
+    const selectedDeleteBtn = document.querySelectorAll('.deleteBtn')
+    selectedDeleteBtn.forEach(deleteBtn => {
+        let identifier = deleteBtn.classList.contains('identified')
+        console.log(identifier)
+        if(identifier == false){
+            deleteBtn.classList.add('identified')
+            identifiedDeleteBtn = document.querySelectorAll('.identified')
+            selectDeleteBtn()
+            console.log("HOla")
+        } else {
+            return
+        }
+    })
+}
 
 
-
+function selectDeleteBtn() {
+    //deleteBookFromArray()
+    //const selectedDeleteBtn = document.querySelectorAll('.deleteBtn')
+    identifiedDeleteBtn.forEach(deleteBtn => {
+        deleteBtn.addEventListener('click', () => {
+            deleteBtn.parentElement.parentElement.remove() 
+        })
+    })
+}
+*/
 
